c8876e91aa0bc841117e1a9e6c59001607c6d97e
newbee <beenewminh@outlook.com>
2019-12-14 18:56:12 +0700

    Restart project from scratch

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,1 @@
-#include "bits/stdc++.h"
-#include "Game.h"
 
-// init game engine
-usr::Game game;
-
-signed main() {
-
-  // Game loop
-  while (game.running()) {
-    // Update
-    game.update();
-
-    // Render
-    game.render();
-  }
-  
-  return 0;
-}
