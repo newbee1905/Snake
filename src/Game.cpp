47a51aadfabb59e115c8fd0afb2ce392d650bf25
newbee1905 <beenewminh@outlook.com>
2019-12-14 13:41:05 +0700

    CMake init and add few cpp files

---
+++ src/Game.cpp
@@ -0,0 +1,85 @@
+#include "bits/stdc++.h"
+
+#include "Game.h"
+
+// set namespace for better syntax when declaring Class name
+// using namespace here for shorter code
+using namespace usr;
+
+// Private Functions
+void Game::initVariables() {
+  this->window = nullptr;
+}
+
+void Game::initWindow() {
+  this->windowSize.width = 800;
+  this->windowSize.height = 600;
+  this->windowTitle = "Game Snake";
+
+  this->window = new sf::RenderWindow(this->windowSize, this->windowTitle, sf::Style::Titlebar | sf::Style::Close);
+
+  this->window->setFramerateLimit(60);
+}
+
+void Game::initEnemies() {
+  this->enemy.setPosition(10.f, 10.f);
+  this->enemy.setSize(sf::Vector2f(100.f, 100.f));
+  this->enemy.setFillColor(sf::Color::Red);
+  this->enemy.setOutlineColor(sf::Color(255, 0, 0, 125));
+  this->enemy.setOutlineThickness(2.f);
+}
+
+// Contructor / Destructor
+Game::Game() {
+  this->initVariables();
+  this->initWindow();
+  this->initEnemies();
+}
+
+Game::~Game() {
+  delete this->window;
+}
+
+// Accessors
+const bool Game::running() const {
+  return this->window->isOpen();
+}
+
+// Functions
+void Game::pollEvents() {
+  // Event polling
+  while (this->window->pollEvent(this->ev)) {
+    switch (this->ev.type) {
+    case sf::Event::Closed:
+      this->window->close();
+      break;
+    }
+  }
+}
+
+void Game::update() {
+  this->pollEvents();
+
+  // mouse pos relative to this->window
+  /* std::cout << "Mouse pos: " */
+  /*           << sf::Mouse::getPosition(*this->window).x << " " */
+  /*           << sf::Mouse::getPosition(*this->window).y << " " */
+  /*           << std::endl; */
+}
+
+void Game::render() {
+  /*
+   return void
+   
+   * clear old frames
+   * render object
+   * display frames in window
+  */
+
+  this->window->clear(sf::Color(255, 255, 255, 255));  // Clear old frames
+
+  // Draw game
+  this->window->draw(this->enemy);
+
+  this->window->display(); // Tell app that window is done drawing
+}
---
+++ src/Game.h
@@ -0,0 +1,44 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+
+#include "bits/stdc++.h"
+
+// set namespace for better syntax when declaring Class name
+namespace usr {
+class Game {
+private:
+  // Variables
+  // * Window
+  sf::RenderWindow *window;
+  sf::VideoMode windowSize;
+  std::string windowTitle;
+  sf::Event ev;
+  // * Game Object
+  sf::RectangleShape enemy;
+
+  // private functions
+  void initVariables();
+  void initWindow();
+  void initEnemies();
+
+public:
+  // Contructor / Destructor
+  Game();
+  virtual ~Game();
+
+  // Accessors
+  const bool running() const; // check if game is running or not
+
+  // Functions
+  void pollEvents();
+  void update();
+  void render();
+};
+} // namespace usr
+#endif /* GAME_H */
